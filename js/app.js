754ba34e0939dad2832c2febbce2532cb938f8b2
cindyweiss <weiss_cindy@yahoo.com>
2019-12-06 18:21:14 -0800

    got handlebars and key word filter working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
+
 const picArr = [];
 const keywordArr = [];
+
 function Pic(picObj) {
   this.title = picObj.title;
   this.description = picObj.description;
@@ -9,49 +11,47 @@ function Pic(picObj) {
   this.image_url = picObj.image_url;
   // picArr.push(this);
 }
+
 Pic.prototype.render = function () {
   let picSource = $('#script-template').html();
   let template = Handlebars.compile(picSource);
   return template(this);
 }
+
 // picArr.forEach(picture => {
 //   $('#pic-template').append(new Pic(picture).render());
 // })
+
 // GET JSON DATA INTO ARRAY 'picArr'
-$.get('./data/page-1.json', data => {
+
+$.get('./data/page-1.json', data => { //  ajax callback function
+
   data.forEach(pic => {
     let pushedObj = new Pic(pic);
     picArr.push(pushedObj);
     // const temp = template(pushedObj);
     $('#pic-template').append(new Pic(pic).render());
-    console.log(pushedObj);
   });
   console.log(picArr);
+
+  //  MAKE KEYWORD ARRAY OF UNIQUES
+
+  picArr.forEach((i) => {
+    if (!keywordArr.includes(i.keyword)) {
+      keywordArr.push(i.keyword);
+      $('#select-filter').append(`<option>${i.keyword}</option>`);
+    }
+  });
+  console.log(keywordArr); // PASSED
+
   // picArr.forEach(picToRender => {
   //   $('#pic-template').append(picToRender);
   // })
 });
-// console.log(picToRender);
-// $('#pic-template').append(pushedObj);
-// if (!keywordArr.includes(pic.keyword)) {
-//   keywordArr.push(pic.keyword);
-// }
-// keywordArr.forEach(i => {
-//   $('#filter').append(`<option>${i}</option>`);
-// });
-// let x = new Pic(pic).render();
-//     $('#pic-template').append(x);
-//  MAKE KEYWORD ARRAY OF UNIQUES
-picArr.forEach((i) => {
-  if (!keywordArr.includes(i.keyword)) {
-    keywordArr.push(i.keyword);
-  }
-})
-keywordArr.forEach(i => {
-  $('#filter').append(`<option>${i}</option>`);
-});
+
 //  LISTEN FOR FILTER MENU CHANGE AND RENDER
-$('#filter').on('change', function () {
+
+$('#select-filter').on('change', function () {
   $('#pic-template').hide();
   picArr.forEach(pic => {
     if (this.value === pic.keyword || this.value === 'default') {
@@ -59,3 +59,24 @@ $('#filter').on('change', function () {
     }
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
